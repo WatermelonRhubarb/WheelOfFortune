0551692e0cd9c1eff5208bb19d6963ac93655de9
Roseanne Damasco <v-rdamasco@microsoft.com>
2021-03-11 12:58:14 -0800

    tested Puzzle constructor

---
+++ WheelOfFortuneTest/PuzzleTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WheelOfFortune;
+
+namespace WheelOfFortuneTest
+{
+    [TestClass]
+    public class PuzzleTest
+    {
+        [TestMethod]
+        public void PuzzleConstructorTest()
+        {
+            Puzzle currentPuzzle = new Puzzle("Welcome to Microsoft Leap");
+            Assert.AreEqual("Welcome to Microsoft Leap", currentPuzzle.PuzzleAnswer);
+            Assert.AreEqual("_______ __ _________ ____", currentPuzzle.PuzzleSoFar);
+        }
+    }
+}
