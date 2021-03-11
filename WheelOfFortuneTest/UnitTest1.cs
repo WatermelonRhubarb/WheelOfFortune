ba5f8d0e31ed6b5cc91171d87aee4eea51cbc752
Yena Choi <yenachoi95@gmail.com>
2021-03-11 13:23:13 -0800

    Added SPA constructor unit test

--- WheelOfFortuneTest/UnitTest1.cs
+++ WheelOfFortuneTest/UnitTest1.cs
@@ -1,13 +1,21 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WheelOfFortune;
 
 namespace WheelOfFortuneTest
 {
     [TestClass]
     public class UnitTest1
     {
+        // Solve Puzzle Action Tests
+        /// <summary>
+        /// Tests that a new instance of the SolvePuzzleAction class initializes the property PuzzleGuess with an empty string.
+        /// </summary>
         [TestMethod]
-        public void TestMethod1()
+        public void SolvePuzzleActionConstructorTest()
         {
+            SolvePuzzleAction spa = new SolvePuzzleAction();
+            Assert.IsTrue(String.IsNullOrEmpty(spa.PuzzleGuess));
         }
     }
 }
