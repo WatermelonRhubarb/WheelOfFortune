7f6d2d8783d943bd5c1a342a32231feac4f23def
Yena Choi <yenachoi95@gmail.com>
2021-03-11 12:58:16 -0800

    Added unit tests for SPA Execute

--- WheelOfFortuneTest/UnitTest1.cs
+++ WheelOfFortuneTest/UnitTest1.cs
@@ -1,13 +1,35 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WheelOfFortune;
 
 namespace WheelOfFortuneTest
 {
     [TestClass]
     public class UnitTest1
     {
+        // Solve Puzzle Action Tests
         [TestMethod]
-        public void TestMethod1()
+        public void ExecuteCorrectSolutionTest()
         {
+            Puzzle currentPuzzle = new Puzzle();
+            currentPuzzle.PuzzleAnswer = "WATERMELON";
+
+            SolvePuzzleAction spa = new SolvePuzzleAction();
+            spa.PuzzleGuess = "WATERMELON";
+
+            Assert.AreEqual(spa.Execute(currentPuzzle), true);
+        }
+
+        [TestMethod]
+        public void ExecuteIncorrectSolutionTest()
+        {
+            Puzzle currentPuzzle = new Puzzle();
+            currentPuzzle.PuzzleAnswer = "WATERMELON";
+
+            SolvePuzzleAction spa = new SolvePuzzleAction();
+            spa.PuzzleGuess = "WATERBERRY";
+
+            Assert.AreEqual(spa.Execute(currentPuzzle), false);
         }
     }
+
 }
