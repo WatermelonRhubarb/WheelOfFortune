58dec6a7f35cb226d9469fb1826950010b2b056b
Yena Choi <yenachoi95@gmail.com>
2021-03-11 23:06:08 -0800

    Update test files

--- WheelOfFortuneTest/SolvePuzzleActionTest.cs
+++ WheelOfFortuneTest/SolvePuzzleActionTest.cs
@@ -20,5 +20,34 @@ namespace WheelOfFortuneTest
             SolvePuzzleAction spa = new SolvePuzzleAction();
             Assert.IsTrue(String.IsNullOrEmpty(spa.PuzzleGuess));
         }
+
+        /// <summary>
+        ///  Tests that the Execute() method of the SolvePuzzleAction returns true when the current guess matches the PuzzleAnswer
+        /// </summary>
+        [TestMethod]
+        public void SolvePuzzleActionExecuteCorrectSolutionTest()
+        {
+            Puzzle currentPuzzle = new Puzzle("WATERMELON");
+
+            SolvePuzzleAction spa = new SolvePuzzleAction();
+            spa.PuzzleGuess = "WATERMELON";
+
+            Assert.IsTrue(spa.Execute(currentPuzzle));
+        }
+
+        /// <summary>
+        /// Tests that the Execute() method of the SolvePuzzleAction returns false when the current guess does not match the PuzzleAnswer
+        /// </summary>
+        [TestMethod]
+        public void SolvePuzzleActionExecuteIncorrectSolutionTest()
+        {
+            Puzzle currentPuzzle = new Puzzle("WATERMELON");
+
+            SolvePuzzleAction spa = new SolvePuzzleAction();
+            spa.PuzzleGuess = "WATERBERRY";
+
+            Assert.IsFalse(spa.Execute(currentPuzzle));
+        }
     }
+
 }
