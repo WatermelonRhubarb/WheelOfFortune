d950bea394f66dd7e17bc1b07703152a65a53835
Yena Choi <yenachoi95@gmail.com>
2021-03-16 21:35:49 -0700

    Added enum ErrorTypes

---
+++ WheelOfFortune/InvalidInputError.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace WheelOfFortune
+{
+    /// <summary>
+    /// Class representing validation errors for player inputs
+    /// </summary>
+    public class InvalidInputError
+    {
+        /// <summary>
+        /// Enumeration for different types of validation errors
+        /// </summary>
+        public enum ErrorTypes
+        {
+            /// <summary>Input was an empty string or null</summary>
+            EmptyInput,
+            /// <summary>Input was a string when a char was expected</summary>
+            ReceivedStringExpectedChar,
+            /// <summary>Input was not 'y' or 'n' (not case sensitive)</summary>
+            InvalidInputYOrN,
+            /// <summary>Input was not '1' or '2'</summary>
+            InvalidInput1Or2,
+        }
+
+    }
+}
